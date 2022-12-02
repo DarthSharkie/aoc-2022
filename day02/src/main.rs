004173651339e65925ded0744f5a588ff3c99d0e
Steven Hood <stevenmhood+github@gmail.com>
2022-12-01 21:39:40 -0800

    Remove muts from day 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -21,46 +21,41 @@ fn main() {
 }
 
 fn part1(games: &[Input]) -> usize {
-
-    let mut total = 0;
-
-    for game in games {
-        let score = match game.as_str() {
-            "A X" => 1 + 3,
-            "A Y" => 2 + 6,
-            "A Z" => 3 + 0,
-            "B X" => 1 + 0,
-            "B Y" => 2 + 3,
-            "B Z" => 3 + 6,
-            "C X" => 1 + 6,
-            "C Y" => 2 + 0,
-            "C Z" => 3 + 3,
-            _ => 0,
-        };
-        total = total + score;
-    }      
-    total
+    games.iter()
+        .map(|game| {
+            match game.as_str() {
+                "A X" => 1 + 3,
+                "A Y" => 2 + 6,
+                "A Z" => 3 + 0,
+                "B X" => 1 + 0,
+                "B Y" => 2 + 3,
+                "B Z" => 3 + 6,
+                "C X" => 1 + 6,
+                "C Y" => 2 + 0,
+                "C Z" => 3 + 3,
+                _ => 0,
+            }
+        })
+        .sum()
 }
 
 fn part2(games: &[Input]) -> usize {
-    let mut total = 0;
-
-    for game in games {
-        let score = match game.as_str() {
-            "A X" => 3 + 0,
-            "A Y" => 1 + 3,
-            "A Z" => 2 + 6,
-            "B X" => 1 + 0,
-            "B Y" => 2 + 3,
-            "B Z" => 3 + 6,
-            "C X" => 2 + 0,
-            "C Y" => 3 + 3,
-            "C Z" => 1 + 6,
-            _ => 0,
-        };
-        total = total + score;
-    }
-    total
+    games.iter()
+        .map(|game| {
+            match game.as_str() {
+                "A X" => 3 + 0,
+                "A Y" => 1 + 3,
+                "A Z" => 2 + 6,
+                "B X" => 1 + 0,
+                "B Y" => 2 + 3,
+                "B Z" => 3 + 6,
+                "C X" => 2 + 0,
+                "C Y" => 3 + 3,
+                "C Z" => 1 + 6,
+                _ => 0,
+            }
+        })
+        .sum()
 }
 
 #[test]
