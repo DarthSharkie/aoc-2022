4f1851fb03bcea5a8601866655f54968b869504c
DarthSharkie <darthsharkie@gmail.com>
2022-12-16 13:02:35 -0800

    Day 14 part 2

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -2,7 +2,6 @@ use std::fs;
 use std::time::Instant;
 use std::io::{self, BufRead};
 use std::collections::HashMap;
-use std::cmp;
 
 type Input = String;
 
@@ -25,7 +24,7 @@ impl Pos {
     fn below_right(&self) -> Self { Pos { x: self.x + 1, y: self.y + 1 } }
 }
 
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 enum Content {
     Air,
     Sand,
@@ -50,22 +49,8 @@ fn read_walls(lines: &[Input]) -> Vec<Vec<Pos>> {
     }).collect()
 }
 
-fn main() {
-    let start = Instant::now();
-    let lines: Vec<Input> = load_file("/mnt/s/AdventOfCode/2022/input14.txt").unwrap();
-    let walls: Vec<Vec<Pos>> = read_walls(&lines);
-
-    println!("Part 1: {}", part1(&walls));
-    println!("Part 2: {}", part2(&walls));
-    let elapsed = start.elapsed();
-    println!("Elapsed: {}µs", elapsed.as_micros());
-}
-
-fn part1(walls: &Vec<Vec<Pos>>) -> usize {
+fn build_grid(walls: &Vec<Vec<Pos>>) -> HashMap<Pos, Content> {
     let mut grid: HashMap<Pos, Content> = HashMap::new();
-
-    // Build the grid from the walls
-    let mut bottom = 0;
     walls.iter().for_each(|wall| {
         //println!("{wall:?}");
         wall.windows(2).for_each(|joint| {
@@ -76,12 +61,32 @@ fn part1(walls: &Vec<Vec<Pos>>) -> usize {
                 block = block.toward(&joint[1]);
                 //println!("{block:?}");
                 grid.insert(block, Content::Rock);
-                // For some reason, sand falls toward greater y-values...
-                bottom = cmp::max(block.y, bottom);
             }
 
         });
     });
+    grid
+}
+
+fn main() {
+    let start = Instant::now();
+    let lines: Vec<Input> = load_file("/mnt/s/AdventOfCode/2022/input14.txt").unwrap();
+    let walls: Vec<Vec<Pos>> = read_walls(&lines);
+
+    let mut grid = build_grid(&walls);
+    println!("Part 1: {}", part1(&mut grid));
+
+    let mut grid = build_grid(&walls);
+    println!("Part 2: {}", part2(&mut grid));
+
+    let elapsed = start.elapsed();
+    println!("Elapsed: {}µs", elapsed.as_micros());
+}
+
+fn part1(grid: &mut HashMap<Pos, Content>) -> usize {
+
+    // For some reason, sand falls toward greater y-values...
+    let bottom = grid.keys().map(|pos| pos.y).max().expect("No grid!");
 
     // Start dropping sand
     let origin = Pos {x: 500, y: 0};
@@ -100,21 +105,49 @@ fn part1(walls: &Vec<Vec<Pos>>) -> usize {
     grains
 }
 
-fn part2(walls: &Vec<Vec<Pos>>) -> usize {
-    0
+fn part2(grid: &mut HashMap<Pos, Content>) -> usize {
+
+    // For some reason, sand falls toward greater y-values...
+    let bottom = grid.keys().map(|pos| pos.y).max().expect("No grid!") + 2;
+    // Fill in the floor
+    let left = 500 - bottom - 5;
+    let right = 500 + bottom + 5;
+    println!("{left} - {right} is the floor");
+    for x in left..=right {
+        grid.insert(Pos { x, y: bottom }, Content::Rock);
+    }
+
+    // Start dropping sand
+    let origin = Pos {x: 500, y: 0};
+    let mut grains = 0;
+    let mut sand = origin.clone();
+    while grid.get(&origin) == None {
+        let options = vec![sand.below(), sand.below_left(), sand.below_right()];
+        if let Some(next) = options.iter().find(|pos| grid.get(pos) == None) {
+            sand = *next;
+            // print!("{sand:?} -> ");
+        } else {
+            //println!("{sand:?}");
+            grid.insert(sand, Content::Sand);
+            grains += 1;
+            sand = origin.clone();
+            //println!("{:?}", grid.get(&sand));
+        }
+    }
+    grains
 }
 
 #[test]
 fn test_part1() {
     let lines: Vec<Input> = load_file("test.txt").unwrap();
-    let grid = read_walls(&lines);
-    assert_eq!(part1(&grid), 0);
+    let mut grid = build_grid(&read_walls(&lines));
+    assert_eq!(part1(&mut grid), 24);
 }
 
 #[test]
 fn test_part2() {
     let lines: Vec<Input> = load_file("test.txt").unwrap();
-    let grid = read_walls(&lines);
-    assert_eq!(part2(&grid), 0);
+    let mut grid = build_grid(&read_walls(&lines));
+    assert_eq!(part2(&mut grid), 93);
 }
 
